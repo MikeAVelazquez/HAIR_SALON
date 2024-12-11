ddd563b073ac798c632f968ae8458c7d11be4244
root <root@Mike-VSG>
2024-12-10 20:21:10 -1000

    name change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return(
     <div>
       <header>
-        <h1>The Best Hair Salon</h1>
+        <h1>Cultivate Beauty</h1>
         <nav>
           <a href="/">Home</a> | <a href="/Staff">Staff</a> | <a href="/Services">Services</a> | <a href="/Booking">Booking</a> | <a href="/Admin">Admin</a>
         </nav>
